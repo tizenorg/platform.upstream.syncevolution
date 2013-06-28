1e1e1085300b3583a397cc1592244818f350f87a
Patrick Ohly <patrick.ohly@intel.com>
2013-06-28 11:34:20 +0000

    Imported Upstream version 0.8

--- src/backends/evolution/EvolutionCalendarSource.cpp
+++ src/backends/evolution/EvolutionCalendarSource.cpp
@@ -1,6 +1,5 @@
 /*
- * Copyright (C) 2005-2006 Patrick Ohly
- * Copyright (C) 2007 Funambol
+ * Copyright (C) 2005-2008 Patrick Ohly
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -201,7 +200,18 @@ void EvolutionCalendarSource::close()
     // with Evolution Exchange Connector: when updating
     // a child event, it seems to take a while until
     // the change really is effective.
-    sleepSinceModification(5);
+    static int secs = 5;
+    static bool checked = false;
+    if (!checked) {
+        // allow setting the delay (used during testing to shorten runtime)
+        const char *delay = getenv("SYNC_EVOLUTION_EVO_CALENDAR_DELAY");
+        if (delay) {
+            secs = atoi(delay);
+        }
+        checked = true;
+    }
+
+    sleepSinceModification(secs);
 
     m_calendar = NULL;
 }
